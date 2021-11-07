447d1c5d4979199221638c1dfabf3d27cfe49c6f
Hector-Carmona <hector.carmona.reyes@gmail.com>
2021-11-06 20:59:28 -0600

    Uploading new changes

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -86,18 +86,19 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     return magnitude * 4;
   }  
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data,{
-    // We turn each feature into a circleMarker on the map
-    pointToLayer: function(feature, latlng){
+ // Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data, {
+  // We turn each feature into a circleMarker on the map.
+  pointToLayer: function(feature, latlng) {
       console.log(data);
       return L.circleMarker(latlng);
     },
   // We set the style for each circleMarker using our styleInfo function.
-  style: styleInfo
+style: styleInfo,
   // We create a popup for each circleMarker to display the magnitude and
   //  location of the earthquake after the marker has been created and styled.
-    onEachFeature: function(feature, layer) {
-      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-    }
-  }).addTo(map);
+  onEachFeature: function(feature, layer) {
+  layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+}
+}).addTo(map);
 });
